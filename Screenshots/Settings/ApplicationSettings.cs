4c020ccb0ca75495c8b0453f6b8a19d72aea40db
Bohdan Marko <bohdan.marko.228@gmail.com>
2023-09-12 20:15:03 +0300

    add Application Settings

---
+++ Screenshots/Settings/ApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Screenshots.Settings;
+
+public sealed class ApplicationSettings
+{
+    public required string BaseFileName { get; set; }
+    public required string TimeStampFormat { get; set; }
+    public required string FontName { get; set; }
+    public required int FontSize { get; set; }
+    public required int ScreenshotHeight { get; set; }
+    public required int ScreenshotWidth { get; set; }
+    public required string CultureName { get; set; }
+}
